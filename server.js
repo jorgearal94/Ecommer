f365f0182a20d01154ca16f4e7746ac1e1b5c387
jorgearal94 <152846404+jorgearal94@users.noreply.github.com>
2023-12-04 01:27:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -23,16 +23,18 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
 
+//rest api
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+});
+  
   //PORT
   const PORT = process.env.PORT || 8080;
 
